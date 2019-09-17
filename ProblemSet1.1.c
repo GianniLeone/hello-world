9cc60649b66b770b588d6166a0ea12059eb4c65f
GianniLeone <55284172+GianniLeone@users.noreply.github.com>
2019-09-17 08:31:25 -0400

    Create ProblemSet1.1.c

---
+++ ProblemSet1.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//A function named "main." Every C program begins with the main() function.
+//"main"is where the program starts when executed.
+int main(int argc, char const *argv[]) {
+  int number = 2019
+  float z = 0.23f
+  char c = 'm'
+  
+  printf('m'); //Print to the terminal window
+  printf(0.23f); 
+  printf(2019);
+  return 0; //Exit status of 0 to signify that the program reached the end
+  }
