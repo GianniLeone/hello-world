fa0f58c316e69dc2072b6cb185a67ea2456207da
GianniLeone <55284172+GianniLeone@users.noreply.github.com>
2019-09-19 18:32:21 -0400

    Create Problemset1.3.c

---
+++ Problemset1.3.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+//A function named "main." Every C program begins with the main() function.
+//"main"is where the program starts when executed.
+int main(void) {
+
+  printf(++++++++++/n ++++++++++/n ++++++++++/n); //Print to the terminal window
+  return 0; //Exit status of 0 to signify that the program reached the end
+  }
