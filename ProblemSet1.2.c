7d1d45f0ecac92ba14b72916a9d568bc0157741d
GianniLeone <55284172+GianniLeone@users.noreply.github.com>
2019-09-19 22:28:06 -0400

    Update ProblemSet1.2.c

--- ProblemSet1.2.c
+++ ProblemSet1.2.c
@@ -1,2 +1,13 @@
 
-system(“afplay -r 2 audiofile.mp3”)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//A function named "main." Every C program begins with the main() function.
+//"main"is where the program starts when executed.
+int main(void) {
+
+  system(“afplay -r 2 audiofile.mp3”)
+  
+  return 0; //Exit status of 0 to signify that the program reached the end
+  }
