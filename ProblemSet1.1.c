095325223aa7df7272a9bb5f390285a930d599f6
GianniLeone <55284172+GianniLeone@users.noreply.github.com>
2019-09-19 22:33:51 -0400

    Update ProblemSet1.1.c

--- ProblemSet1.1.c
+++ ProblemSet1.1.c
@@ -11,3 +11,14 @@ int main(void) {
   printf("%c, %f, %d", c, z, year); //Print to the terminal window
   return 0; //Exit status of 0 to signify that the program reached the end
   }
+
+
+//Project Name: Problem Set 1.2
+//Submitted by: Gianni Leone
+//For Course: Audio Programing In C
+//Due Date: 10/20/19
+//Files included: N/A
+//Purpose: Practice
+//Build and Run commands
+//Caveats, if any
+//Acknowledgement, if any
