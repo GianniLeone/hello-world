f87d7bc2732779a778f81e9f9e6385d94703701f
GianniLeone <55284172+GianniLeone@users.noreply.github.com>
2019-09-19 22:34:47 -0400

    Update Problemset1.3.c

--- Problemset1.3.c
+++ Problemset1.3.c
@@ -9,3 +9,14 @@ int main(void) {
   printf("++++++++++\n++++++++++\n++++++++++\n"); //Print to the terminal window
   return 0; //Exit status of 0 to signify that the program reached the end
   }
+
+
+//Project Name: Problem Set 1
+//Submitted by: Gianni Leone
+//For Course: Audio Programing In C
+//Due Date: 10/20/19
+//Files included: N/A
+//Purpose: Practice
+//Build and Run commands
+//Caveats, if any
+//Acknowledgement, if any
