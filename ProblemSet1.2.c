416eaf031a17c42212e8efa254c4ae8f239570d6
GianniLeone <55284172+GianniLeone@users.noreply.github.com>
2019-09-19 22:33:17 -0400

    Update ProblemSet1.2.c

--- ProblemSet1.2.c
+++ ProblemSet1.2.c
@@ -7,7 +7,17 @@
 //"main"is where the program starts when executed.
 int main(void) {
 
-  system(“afplay -r 2 audiofile.mp3”)
+  system(“afplay -r 2 wavyairr.mp3”)
   
   return 0; //Exit status of 0 to signify that the program reached the end
   }
+
+//Project Name: Problem Set 1
+//Submitted by: Gianni Leone
+//For Course: Audio Programing In C
+//Due Date: 10/20/19
+//Files included: N/A
+//Purpose: Practice
+//Build and Run commands
+//Caveats, if any
+//Acknowledgement, if any
